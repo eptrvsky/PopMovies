a4d6b9a14c4d0224bd11f07dd77311aeea1b3fc3
eptrvsky <eptrvsky@gmail.com>
2015-08-11 13:01:52 +0300

    Initial commit

---
+++ app/src/main/java/com/yellow/evgeny/popmovies/MainActivityFragment.java
@@ -0,0 +1,22 @@
+package com.yellow.evgeny.popmovies;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Evgeny on 11.08.2015.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
