a504bbd8165c72331015ae018e871f3c2cc98182
eptrvsky <eptrvsky@gmail.com>
2015-08-11 15:07:43 +0300

    11_08_15[15:00]

--- app/src/main/java/com/yellow/evgeny/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/yellow/evgeny/popmovies/MainActivityFragment.java
@@ -17,6 +17,15 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView= inflater.inflate(R.layout.fragment_main, container, false);
+        String[] forecastArray = {
+                "Title,YEAR,120min",
+                "Trailer",
+                "Trailer",
+                "Trailer",
+                "Trailer",
+                "Trailer",
+        };
+        return rootView;
     }
 }
